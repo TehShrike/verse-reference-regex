fa13ebe88dd378560bce5194e6b95cd79bd6de24
Josh Duff <me@JoshDuff.com>
2017-03-30 14:16:37 -0500

    More verbose test assertions

--- test.js
+++ test.js
@@ -1,79 +1,285 @@
 const test = require('tape')
 const { createRegex, extractRangeFromMatch } = require('./')
 
-function r(book, start, end = null) {
-	return {
-		book,
-		start,
-		end: end === null ? start : {
-			chapter: end.chapter === null ? start.chapter : end.chapter,
-			verse: end.verse === null ? start.verse : end.verse,
-			section: end.verse === null ? start.section : end.section,
-		},
-	}
-}
-
-function v(chapter, verse = null, section = null) {
-	return {
-		chapter,
-		verse,
-		section
-	}
-}
-
 const hasReferenceWithVerse = [
-	{ text: `Revelation 13:3`, range: r('Revelation', v(13, 3)) },
-	{ text: `Look it up on Genesis 3:55`, range: r('Genesis', v(3, 55)) },
-	{ text: `Test the Romans 44:559 out`, range: r('Romans', v(44, 559)) },
-	{ text: `Low in \nthe philippians 366:4 he lay`, range: r('Philippians', v(366, 4)) },
-	{ text: `An abbreviation is Ps 13:4 y'know`, range: r('Psalms', v(13, 4)) },
-	{ text: `Another abbreviation is Ps. 44:8`, range: r('Psalms', v(44, 8)) },
-	{ text: `Verse section: 1 sam 12:4b`, range: r('1 Samuel', v(12, 4, 'b')) },
+	{
+		text: `Revelation 13:3`,
+		expected: {
+			book: 'Revelation',
+			start: { chapter: 13, verse: 3, section: null },
+			end: { chapter: 13, verse: 3, section: null }
+		}
+	}, {
+		text: `Look it up on Genesis 3:55`,
+		expected: {
+			book: 'Genesis',
+			start: { chapter: 3, verse: 55, section: null },
+			end: { chapter: 3, verse: 55, section: null }
+		}
+	}, {
+		text: `Test the Romans 44:559 out`,
+		expected: {
+			book: 'Romans',
+			start: { chapter: 44, verse: 559, section: null },
+			end: { chapter: 44, verse: 559, section: null }
+		}
+	}, {
+		text: `Low in \nthe philippians 366:4 he lay`,
+		expected: {
+			book: 'Philippians',
+			start: { chapter: 366, verse: 4, section: null },
+			end: { chapter: 366, verse: 4, section: null }
+		}
+	}, {
+		text: `An abbreviation is Ps 13:4 y'know`,
+		expected: {
+			book: 'Psalms',
+			start: { chapter: 13, verse: 4, section: null },
+			end: { chapter: 13, verse: 4, section: null }
+		}
+	}, {
+		text: `Another abbreviation is Ps. 44:8`,
+		expected: {
+			book: 'Psalms',
+			start: { chapter: 44, verse: 8, section: null },
+			end: { chapter: 44, verse: 8, section: null }
+		}
+	}, {
+		text: `Verse section: 1 sam 12:4b`,
+		expected: {
+			book: '1 Samuel',
+			start: { chapter: 12, verse: 4, section: 'b' },
+			end: { chapter: 12, verse: 4, section: 'b' }
+		}
+	}, {
+		text: `Second bookname: 2nd sam 12:4b`,
+		expected: {
+			book: '2 Samuel',
+			start: { chapter: 12, verse: 4, section: 'b' },
+			end: { chapter: 12, verse: 4, section: 'b' }
+		}
+	},
 ]
 
 const hasReferenceWithoutVerse = [
-	{ text: `Revelation 13`, range: r('Revelation', v(13)) },
-	{ text: `Look it up on Genesis 3: book`, range: r('Genesis', v(3)) },
-	{ text: `Test the Romans 44 out`, range: r('Romans', v(44)) },
-	{ text: `Low in \nthe philippians 366 he lay`, range: r('Philippians', v(366)) },
-	{ text: `An abbreviation is Ps 13 y'know`, range: r('Psalms', v(13)) },
-	{ text: `Another abbreviation is Ps. 44`, range: r('Psalms', v(44)) },
+	{
+		text: `Revelation 13`,
+		expected: {
+			book: 'Revelation',
+			start: { chapter: 13, verse: null, section: null },
+			end: { chapter: 13, verse: null, section: null }
+		}
+	}, {
+		text: `Look it up on Genesis 3: book`,
+		expected: {
+			book: 'Genesis',
+			start: { chapter: 3, verse: null, section: null },
+			end: { chapter: 3, verse: null, section: null }
+		}
+	}, {
+		text: `Test the Romans 44 out`,
+		expected: {
+			book: 'Romans',
+			start: { chapter: 44, verse: null, section: null },
+			end: { chapter: 44, verse: null, section: null }
+		}
+	}, {
+		text: `Low in \nthe philippians 366 he lay`,
+		expected: {
+			book: 'Philippians',
+			start: { chapter: 366, verse: null, section: null },
+			end: { chapter: 366, verse: null, section: null }
+		}
+	}, {
+		text: `An abbreviation is Ps 13 y'know`,
+		expected: {
+			book: 'Psalms',
+			start: { chapter: 13, verse: null, section: null },
+			end: { chapter: 13, verse: null, section: null }
+		}
+	}, {
+		text: `Another abbreviation is Ps. 44`,
+		expected: {
+			book: 'Psalms',
+			start: { chapter: 44, verse: null, section: null },
+			end: { chapter: 44, verse: null, section: null }
+		}
+	},
 ]
 
 const hasRangeWithVerse = [
-	{ text: `Revelation 13:3-14:4`, range: r('Revelation', v(13, 3), v(14, 4)) },
-	{ text: `Look it up on Genesis 3:55-23:44`, range: r('Genesis', v(3, 55), v(23, 44)) },
-	{ text: `Test the Romans 44:559-1:1 out`, range: r('Romans', v(44, 559), v(1, 1)) },
-	{ text: `Low in \nthe philippians 366:4-12:12 he lay`, range: r('Philippians', v(366, 4), v(12, 12)) },
-	{ text: `An abbreviation is Ps 13:4-19:19 y'know`, range: r('Psalms', v(13, 4), v(19, 19)) },
-	{ text: `Another abbreviation is Ps. 44:8-3:3`, range: r('Psalms', v(44, 8), v(3, 3)) },
-	{ text: `Verse section: 1 sam 12:4b-13:5a`, range: r('1 Samuel', v(12, 4, 'b'), v(13, 5, 'a')) },
+	{
+		text: `Revelation 13:3-14:4`,
+		expected: {
+			book: 'Revelation',
+			start: { chapter: 13, verse: 3, section: null },
+			end: { chapter: 14, verse: 4, section: null }
+		}
+	}, {
+		text: `Look it up on Genesis 3:55-23:44`,
+		expected: {
+			book: 'Genesis',
+			start: { chapter: 3, verse: 55, section: null },
+			end: { chapter: 23, verse: 44, section: null }
+		}
+	}, {
+		text: `Test the Romans 44:559-1:1 out`,
+		expected: {
+			book: 'Romans',
+			start: { chapter: 44, verse: 559, section: null },
+			end: { chapter: 1, verse: 1, section: null }
+		}
+	}, {
+		text: `Low in \nthe philippians 366:4-12:12 he lay`,
+		expected: {
+			book: 'Philippians',
+			start: { chapter: 366, verse: 4, section: null },
+			end: { chapter: 12, verse: 12, section: null }
+		}
+	}, {
+		text: `An abbreviation is Ps 13:4-19:19 y'know`,
+		expected: {
+			book: 'Psalms',
+			start: { chapter: 13, verse: 4, section: null },
+			end: { chapter: 19, verse: 19, section: null }
+		}
+	}, {
+		text: `Another abbreviation is Ps. 44:8-3:3`,
+		expected: {
+			book: 'Psalms',
+			start: { chapter: 44, verse: 8, section: null },
+			end: { chapter: 3, verse: 3, section: null }
+		}
+	}, {
+		text: `Verse section: 1 sam 12:4b-13:5a`,
+		expected: {
+			book: '1 Samuel',
+			start: { chapter: 12, verse: 4, section: 'b' },
+			end: { chapter: 13, verse: 5, section: 'a' }
+		}
+	},
 
-	{ text: `Revelation 13:3-4`, range: r('Revelation', v(13, 3), v(13, 4)) },
-	{ text: `Look it up on Genesis 3:55-44`, range: r('Genesis', v(3, 55), v(3, 44)) },
-	{ text: `Test the Romans 44:559-1 out`, range: r('Romans', v(44, 559), v(44, 1)) },
-	{ text: `Low in \nthe philippians 366:4-12 he lay`, range: r('Philippians', v(366, 4), v(366, 12)) },
-	{ text: `An abbreviation is Ps 13:4-19 y'know`, range: r('Psalms', v(13, 4), v(13, 19)) },
-	{ text: `Another abbreviation is Ps. 44:8-3`, range: r('Psalms', v(44, 8), v(44, 3)) },
-	{ text: `Verse section: 1 sam 12:4b-5a`, range: r('1 Samuel', v(12, 4, 'b'), v(12, 5, 'a')) },
+	{
+		text: `Revelation 13:3-4`,
+		expected: {
+			book: 'Revelation',
+			start: { chapter: 13, verse: 3, section: null },
+			end: { chapter: 13, verse: 4, section: null }
+		}
+	}, {
+		text: `Look it up on Genesis 3:55-44`,
+		expected: {
+			book: 'Genesis',
+			start: { chapter: 3, verse: 55, section: null },
+			end: { chapter: 3, verse: 44, section: null }
+		}
+	}, {
+		text: `Test the Romans 44:559-1 out`,
+		expected: {
+			book: 'Romans',
+			start: { chapter: 44, verse: 559, section: null },
+			end: { chapter: 44, verse: 1, section: null }
+		}
+	}, {
+		text: `Low in \nthe philippians 366:4-12 he lay`,
+		expected: {
+			book: 'Philippians',
+			start: { chapter: 366, verse: 4, section: null },
+			end: { chapter: 366, verse: 12, section: null }
+		}
+	}, {
+		text: `An abbreviation is Ps 13:4-19 y'know`,
+		expected: {
+			book: 'Psalms',
+			start: { chapter: 13, verse: 4, section: null },
+			end: { chapter: 13, verse: 19, section: null }
+		}
+	}, {
+		text: `Another abbreviation is Ps. 44:8-3`,
+		expected: {
+			book: 'Psalms',
+			start: { chapter: 44, verse: 8, section: null },
+			end: { chapter: 44, verse: 3, section: null }
+		}
+	}, {
+		text: `Verse section: 1 sam 12:4b-5a`,
+		expected: {
+			book: '1 Samuel',
+			start: { chapter: 12, verse: 4, section: 'b' },
+			end: { chapter: 12, verse: 5, section: 'a' }
+		}
+	},
 
-	{ text: `Verse section: 1 sam 12:4b-a`, range: r('1 Samuel', v(12, 4, 'b'), v(12, 4, 'a')) },
+	{
+		text: `Verse section: 1 sam 12:4b-a`,
+		expected: {
+			book: '1 Samuel',
+			start: { chapter: 12, verse: 4, section: 'b' },
+			end: { chapter: 12, verse: 4, section: 'a' }
+		}
+	},
 ]
 
 const hasRangeWithoutVerse = [
-	{ text: `Revelation 13-14`, range: r('Revelation', v(13), v(14)) },
-	{ text: `Look it up on Genesis 3-99 book`, range: r('Genesis', v(3), v(99)) },
-	{ text: `Test the Romans 44-45 out`, range: r('Romans', v(44), v(45)) },
-	{ text: `Low in \nthe philippians 366-78 he lay`, range: r('Philippians', v(366), v(78)) },
-	{ text: `An abbreviation is Ps 13-18 y'know`, range: r('Psalms', v(13), v(18)) },
-	{ text: `Another abbreviation is Ps. 44-49`, range: r('Psalms', v(44), v(49)) },
+	{
+		text: `Revelation 13-14`,
+		expected: {
+			book: 'Revelation',
+			start: { chapter: 13, verse: null, section: null },
+			end: { chapter: 14, verse: null, section: null }
+		}
+	}, {
+		text: `Look it up on Genesis 3-99 book`,
+		expected: {
+			book: 'Genesis',
+			start: { chapter: 3, verse: null, section: null },
+			end: { chapter: 99, verse: null, section: null }
+		}
+	}, {
+		text: `Test the Romans 44-45 out`,
+		expected: {
+			book: 'Romans',
+			start: { chapter: 44, verse: null, section: null },
+			end: { chapter: 45, verse: null, section: null }
+		}
+	}, {
+		text: `Low in \nthe philippians 366-78 he lay`,
+		expected: {
+			book: 'Philippians',
+			start: { chapter: 366, verse: null, section: null },
+			end: { chapter: 78, verse: null, section: null }
+		}
+	}, {
+		text: `An abbreviation is Ps 13-18 y'know`,
+		expected: {
+			book: 'Psalms',
+			start: { chapter: 13, verse: null, section: null },
+			end: { chapter: 18, verse: null, section: null }
+		}
+	}, {
+		text: `Another abbreviation is Ps. 44-49`,
+		expected: {
+			book: 'Psalms',
+			start: { chapter: 44, verse: null, section: null },
+			end: { chapter: 49, verse: null, section: null }
+		}
+	},
 ]
 
 const hasNoReference = [
-	{ text: `Pumpkin pie`, range: null },
-	{ text: `Test the Romans out`, range: null },
-	{ text: `Low in \nthe philharmonic 366:4 he lay`, range: null },
-	{ text: `An abbreviation is Ps y'know 44:8`, range: null },
+	{
+		text: `Pumpkin pie`,
+		expected: null
+	}, {
+		text: `Test the Romans out`,
+		expected: null
+	}, {
+		text: `Low in \nthe philharmonic 366:4 he lay`,
+		expected: null
+	}, {
+		text: `An abbreviation is Ps y'know 44:8`,
+		expected: null
+	},
 ]
 
 const hasVerse = [ ...hasReferenceWithVerse, ...hasRangeWithVerse ]
@@ -90,8 +296,8 @@ function runAllTests(description, testCaseTester) {
 		tests.forEach(({ options, shouldMatch, shouldNotMatch }) => {
 			t.test(`Testing options: ${JSON.stringify(options)}`, t => {
 				const regex = createRegex(options)
-				testCaseTester({ t, expected: true, regex, cases: shouldMatch, options })
-				testCaseTester({ t, expected: false, regex, cases: shouldNotMatch, options })
+				testCaseTester({ t, expectedToFindMatch: true, regex, cases: shouldMatch, options })
+				testCaseTester({ t, expectedToFindMatch: false, regex, cases: shouldNotMatch, options })
 				t.end()
 			})
 		})
@@ -100,23 +306,23 @@ function runAllTests(description, testCaseTester) {
 	})
 }
 
-runAllTests('Matches', ({ t, expected, regex, cases }) => {
-	const expectation = expected ? 'Should' : 'Should not'
+runAllTests('Matches', ({ t, expectedToFindMatch, regex, cases }) => {
+	const expectation = expectedToFindMatch ? 'Should' : 'Should not'
 	cases.forEach(({ text }) => {
-		t.equal(regex.test(text), expected, `${expectation} match '${text}'`)
+		t.equal(regex.test(text), expectedToFindMatch, `${expectation} match '${text}'`)
 	})
 })
 
-runAllTests('Capturing', ({ t, expected, regex, cases }) => {
-	cases.forEach(({ text, range }) => {
+runAllTests('Capturing', ({ t, expectedToFindMatch, regex, cases }) => {
+	cases.forEach(({ text, expected }) => {
 		const match = text.match(regex)
 
-		if (expected) {
+		if (expectedToFindMatch) {
 			const output = extractRangeFromMatch(match)
 
-			t.equal(output.book, range.book, `Find book name in '${text}'`)
-			t.deepEqual(output.start, range.start, `Find range start in '${text}'`)
-			t.deepEqual(output.end, range.end, `Find range end in '${text}'`)
+			t.equal(output.book, expected.book, `Find book name in '${text}'`)
+			t.deepEqual(output.start, expected.start, `Find range start in '${text}'`)
+			t.deepEqual(output.end, expected.end, `Find range end in '${text}'`)
 		} else {
 			t.equal(match, null, `No match found in ${text}`)
 		}
