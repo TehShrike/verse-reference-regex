32c1c12c5dc0cb4e282a75bbfebb0d1bb18170bb
Josh Duff <me@JoshDuff.com>
2018-09-10 15:36:38 -0500

    Dropping the ESM export because it wasn't good
    It contained CommonJS in dependencies, and also there is an import of the default books-of-the-bible dataset that isn't great for bundling

--- index.es6.js
+++
@@ -1,9 +0,0 @@
-import createRegex from './create-regex.js'
-import extractRangeFromMatch from './extract-range-from-match.js'
-import createChapterVerseRangeRegex from './create-chapter-verse-range-regex.js'
-
-export {
-	createRegex,
-	extractRangeFromMatch,
-	createChapterVerseRangeRegex,
-}
